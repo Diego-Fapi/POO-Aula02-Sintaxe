efdd2ea67e7a6406e057fd6d2bce4f20e5e28374
Aluno <Aluno@fapi.local>
2019-03-07 20:24:48 -0300

    Alteração feita no dia 07/02/2019

--- src/Ex02.java
+++ src/Ex02.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
 
 public class Ex02 {
 
 	public static void main(String[] args) {
 		// TODO: Pendente implementação do exercício
+		// TODO: Mais tarefas a fazer
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int n1, n2;
+		System.out.printf("Informe um primeiro número:");
+		n1 = ler.nextInt();
+		
+		System.out.printf("Informe um segundo número:");
+		n2 = ler.nextInt();
+		
+		
+		if (n1 < n2) {
+			System.out.printf("Esse número é menor: " + n1);
+		} 
+		
+		if (n2 < n1) {
+				System.out.printf("Esse número é menor: " + n2);
+		}
+		
+	
+	
 	}
 
 }
