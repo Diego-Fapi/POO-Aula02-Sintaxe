49a584dddd6b1f6f2db37167bfdcd4b4425e9a8e
Aluno <Aluno@fapi.local>
2019-03-07 21:29:45 -0300

    Falta terminar

---
+++ src/Ex04.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int n;
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Digite o " + i + " número: ");
+			n = ler.nextInt();
+		}
+		
+		
+	}
+
+}
