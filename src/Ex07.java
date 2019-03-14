54b9f84151a93f819b3c1cd9096398f0f39cce84
Aluno <Aluno@fapi.local>
2019-03-14 17:39:54 -0300

    Exercício Finalizado

---
+++ src/Ex07.java
@@ -0,0 +1,15 @@
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x;
+		
+		for(x=0;x<10;x++) {
+			System.out.println("Olá, eu sou uma estrutura de repetição (" + x + "x)");	
+		}
+		
+	}
+
+}
