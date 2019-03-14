a3d2bf1f71f8e70bd7cee36b2a4cec1eb95db95b
Aluno <Aluno@fapi.local>
2019-03-14 17:29:58 -0300

    Exercício Finalizado

---
+++ src/Ex05.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int cod;
+		
+		System.out.println("1- A Vista no dinheiro");
+		System.out.println("2- A vista no débito");
+		System.out.println("3- 1x no cartão de crédito");
+		System.out.println("4- 3x vezes com juros no cartão de crédito");
+		System.out.println("5- 6x no cartão de crédito");
+		
+		System.out.println("Escolha uma das opções abaixo:");
+		cod = ler.nextInt();
+		switch(cod) {
+		case 1:
+			System.out.println("Desconto de 12%");
+		break;
+		case 2:
+			System.out.println("Desconto de 3%");
+			break;
+		case 3:
+			System.out.println("Mesmo preço");
+			break;
+		case 4:
+			System.out.println("Acréscimo de 5%");
+			break;
+		case 5:
+			System.out.println("Acréscimo de 10%");
+			break;
+		default:
+			System.out.println("Essa opção não exite");
+			
+		}
+	}
+}
