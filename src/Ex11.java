02aadff60babd66fa76655ae33ef0ceffd651332
Aluno <Aluno@fapi.local>
2019-03-14 19:02:42 -0300

    Exercício Finalizado

---
+++ src/Ex11.java
@@ -0,0 +1,17 @@
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x;
+		
+		for(x=0;x<=1000;x+=2) {
+			if(x % 3 == 0) {
+				System.out.println("O número " + x + " é par e múltiplo de três.");
+			}
+		}
+		
+	}
+
+}
