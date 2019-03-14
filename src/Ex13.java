9ea8553ac3ccc8f71dfb5cf391b3b028dddadcf0
Aluno <Aluno@fapi.local>
2019-03-14 19:17:28 -0300

    Exercício Finalizado

---
+++ src/Ex13.java
@@ -0,0 +1,18 @@
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x;
+		
+		for(x=100;x>=0;x-=2) {
+			if(x % 2 == 0) {
+				System.out.println("O número " + x + " é par.");
+			}
+			
+		}
+		
+	}
+
+}
