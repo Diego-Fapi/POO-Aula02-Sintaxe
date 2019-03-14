e1410ba2f7a25246215bc722926a9af966a2661e
Aluno <Aluno@fapi.local>
2019-03-14 17:46:03 -0300

    Exercício Finalizado

---
+++ src/Ex09.java
@@ -0,0 +1,19 @@
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x;
+		int result=0;
+		
+		
+		for(x=1;x<11;x++) {	
+			result=result+x;	
+		}
+		
+		System.out.println("Total da soma: " + result);
+		
+	}
+
+}
