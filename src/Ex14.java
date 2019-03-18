ce68634ca2df230dcb60b94a7230e9fc852dd537
Aluno <Aluno@fapi.local>
2019-03-18 18:46:43 -0300

    Exercício Finalizado

---
+++ src/Ex14.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int x;
+		
+		for(x=-1000;x<=0;x++) {
+			if(x % 2 != 0 && x % 9 == 0) {
+				System.out.println("O número " + x + " é ímpar e múltiplo de nove.");
+			}
+		}
+
+	}
+
+}
